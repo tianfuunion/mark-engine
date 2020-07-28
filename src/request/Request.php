826fc2cd870ff9ce62de7708e1972ccebad2874d
zongdongyang <zongdongyang@126.com>
2020-07-28 21:22:27 +0800

    修复获取顶级域名可能为空的情况

--- src/request/Request.php
+++ src/request/Request.php
@@ -420,11 +420,15 @@
          * @param string $url
          * @param bool $port
          *
-         * @return string
+         * @return string|null
          */
         public static function domain($url = "", $port = false)
         {
-            $host = strtolower(!empty($url) ? parse_url($url, PHP_URL_HOST) : $_SERVER['HTTP_HOST']);
+            $host = !empty($url) ? parse_url($url, PHP_URL_HOST) : $_SERVER['HTTP_HOST'] ?? '';
+            if (empty($host)) {
+                return null;
+            }
+            $host = strtolower($host);
 
             //查看是几级域名
             $data = explode('.', $host);
